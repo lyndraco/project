d324a636a247340df117542d44f0ec3f9a9c2a09
Jessica Yann <yannjess@msu.edu>
2017-01-21 10:28:37 -0500

    playing with logo width

--- js/tg-sample-nav.js
+++ js/tg-sample-nav.js
@@ -3,7 +3,7 @@
 $(function() {
 
 
-	$("body").prepend("<div class='tg-sample-nav'><div class='tg-logo'><img src='img/MI_Arch.jpg.jpg' style='width:200px; height:100px;'></div>"
+	$("body").prepend("<div class='tg-sample-nav'><div class='tg-logo'><img src='img/MI_Arch.jpg.jpg' style='width:1000px; height:100px;'></div>"
 	+ "<ul>"
 	+ "<li><a href='basic.html'>Introduction</a></li>"
 	+ "<li><a href='kitchen_sink.html'>Glossary</a></li>"
