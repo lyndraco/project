a4efa137868a525c64596b0ca6badb8b6baf2a7f
Jessica Yann <yannjess@msu.edu>
2017-01-27 13:02:57 -0500

    file name change

--- js/tg-sample-nav.js
+++ js/tg-sample-nav.js
@@ -5,7 +5,7 @@ $(function() {
 
 	$("body").prepend("<div class='tg-sample-nav'><div class='tg-logo'><a href='index.html'> <img src='img/MI_Arch.jpg.jpg' style='width:200px; height:100px;'> </a></div>"
 	+ "<ul>"
-	+ "<li><a href='basic.html'>Introduction</a></li>"
+	+ "<li><a href='intro.html'>Introduction</a></li>"
 	+ "<li><a href='glossary.html'>Glossary</a></li>"
 	+ "<li><a href='references.html'>References</a></li>"
 	+ "<li><a href='more_info.html'>More information</a></li>"
