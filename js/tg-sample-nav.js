ac3b92ce8c24ac89b3d898eeea06f74517f0196b
Jessica Yann <yannjess@msu.edu>
2017-01-27 14:43:10 -0500

    link to home page

--- js/tg-sample-nav.js
+++ js/tg-sample-nav.js
@@ -5,6 +5,7 @@ $(function() {
 
 	$("body").prepend("<div class='tg-sample-nav'><div class='tg-logo'><a href='index.html'> <img src='img/atlatl_logo.jpg' style='width:100%; height:100px;'> </a></div>"
 	+ "<ul>"
+	+ "<li><a href='index.html'>Home</a></li>"	  
 	+ "<li><a href='intro.html'>Introduction</a></li>"
 	+ "<li><a href='glossary.html'>Glossary</a></li>"
 	+ "<li><a href='references.html'>References</a></li>"
