1591a458d95470859fa28799dadddb371ba79c3a
Jessica Yann <yannjess@msu.edu>
2017-01-27 12:53:09 -0500

    glossary file name

--- js/tg-sample-nav.js
+++ js/tg-sample-nav.js
@@ -6,7 +6,7 @@ $(function() {
 	$("body").prepend("<div class='tg-sample-nav'><div class='tg-logo'><a href='index.html'> <img src='img/MI_Arch.jpg.jpg' style='width:200px; height:100px;'> </a></div>"
 	+ "<ul>"
 	+ "<li><a href='basic.html'>Introduction</a></li>"
-	+ "<li><a href='kitchen_sink.html'>Glossary</a></li>"
+	+ "<li><a href='glossary.html'>Glossary</a></li>"
 	+ "<li><a href='presentation.html'>References</a></li>"
 	+ "<li><a href='custom_tags.html'>More information</a></li>"
 + "</ul></div>");
