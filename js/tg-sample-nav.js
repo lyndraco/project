b4293b7f59ccfd7328613df0991c5f5cb4d62411
Jessica Yann <yannjess@msu.edu>
2017-01-27 13:21:22 -0500

    new image

--- js/tg-sample-nav.js
+++ js/tg-sample-nav.js
@@ -3,7 +3,7 @@
 $(function() {
 
 
-	$("body").prepend("<div class='tg-sample-nav'><div class='tg-logo'><a href='index.html'> <img src='img/MI_Arch.jpg.jpg' style='width:200px; height:100px;'> </a></div>"
+	$("body").prepend("<div class='tg-sample-nav'><div class='tg-logo'><a href='index.html'> <img src='img/atlatl.jpg' style='width:2000px; height:100px;'> </a></div>"
 	+ "<ul>"
 	+ "<li><a href='intro.html'>Introduction</a></li>"
 	+ "<li><a href='glossary.html'>Glossary</a></li>"
