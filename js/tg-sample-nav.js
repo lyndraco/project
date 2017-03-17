a9cbbc6aa9977c2ed10d3f20200a805ca370fffa
Jessica Yann <yannjess@msu.edu>
2017-03-17 13:48:21 -0400

    Update tg-sample-nav.js

--- js/tg-sample-nav.js
+++ js/tg-sample-nav.js
@@ -3,7 +3,7 @@
 $(function() {
 
 
-	$("body").prepend("<div class='tg-sample-nav'><div class='tg-logo'><a href='index.html'> <img src='img/atlatl_logo.jpg' style='width:100%; height:100px;'> </a></div>"
+	$("body").prepend("<div class='tg-sample-nav'><div class='tg-logo'><a href='index.html'> <img src='img/birdstone.jpg' style='width:100%; height:100px;'> </a></div>"
 	+ "<ul>"
 	+ "<li><a href='index.html'>Home</a></li>"	  
 	+ "<li><a href='intro.html'>Introduction</a></li>"
